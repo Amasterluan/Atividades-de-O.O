f6d46711ba17a5cadf7c2e4035eb2591360244df
Luan Moreira <110183580+Amasterluan@users.noreply.github.com>
2022-09-19 22:25:56 -0300

    Create Front.java

---
+++ 16-09/Front.java
@@ -0,0 +1,33 @@
+package Front;
+
+import java.util.Scanner;
+
+import Inform.Funcionario;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		Funcionario e = new Funcionario();
+		Scanner sc = new Scanner (System.in);
+		
+		System.out.print("Digite a matricula: ");		
+		e.setMatricula(sc.nextInt());
+		
+		System.out.print("Digite o nome do Funcionario: ");
+		e.setNome(sc.toString());
+		
+		System.out.print("Digite o salário bruto: ");
+		e.setSal_brut(sc.nextDouble());
+		
+		System.out.print("Digite o Nº de Dependente: ");
+		e.setNum_dependente(sc.nextInt());
+		
+		
+		System.out.println(e.toString());
+	
+		
+	sc.close();
+	}
+
+}
