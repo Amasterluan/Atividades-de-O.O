bd876576398674ca4ad21ab4c2a459f8f677c67f
Luan Moreira <110183580+Amasterluan@users.noreply.github.com>
2022-09-23 12:10:25 -0300

    Update Principal.java

--- 16-09/Principal.java
+++ 16-09/Principal.java
@@ -1,38 +1,30 @@
-package banco;
+package Front;
 
 import java.util.Scanner;
 
-public class front {
+import Inform.Funcionario;
 
+public class Principal {
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+		Funcionario e = new Funcionario();
+		Scanner sc = new Scanner (System.in);
 		
-		conta e = new conta();
+		System.out.print("Digite a matricula: ");		
+		e.setMatricula(sc.nextInt());
 		
-		System.out.print("Digite o numero da conta: ");		
-		e.setNumero(sc.nextInt());
+		System.out.print("Digite o salário bruto: ");
+		e.setSal_brut(sc.nextDouble());
 		
-		System.out.print("Digite o limite: ");
-		e.setLimite(sc.nextDouble());
+		System.out.print("Digite o Nº de Dependente: ");
+		e.setNum_dependente(sc.nextInt());
 		
-		System.out.print("Digite o seu saldo: ");
-		e.setSaldo(sc.nextInt());
-		
-		System.out.println("Digite o valor do saque: ");
-		e.Saque(sc.nextDouble());
-		
-		System.out.println("Digite o valor do deposito: ");
-		e.Deposito(sc.nextDouble());
-		
-		System.out.print("Digite o seu nome: ");
+		System.out.print("Digite o nome do Funcionario: ");
 		e.setNome(sc.next());
-		
-		
+
 		e.exibirDados();
 		
 		sc.close();
 		
-	}
+		}
+
 }
-
-
