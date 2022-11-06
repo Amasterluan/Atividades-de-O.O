ae5668f3133f233c98559c4e2563b7c49188041e
Luan Moreira <110183580+Amasterluan@users.noreply.github.com>
2022-11-06 11:32:04 -0300

    Create Loja.java

---
+++ 04-01/Atividade Av./Questão 1/Loja.java
@@ -0,0 +1,69 @@
+package Q1;
+
+import java.util.ArrayList;
+
+public class Loja {
+
+	private String CNPJ;
+	private String nome;
+	private ArrayList<Proprietario> proprietario;
+	private int telefone;
+	private ArrayList<Computador> computadores;
+	
+	public Loja() {
+		this.proprietario = new ArrayList<Proprietario>();
+		this.computadores = new ArrayList<Computador>();		
+	}
+	
+	
+	public void addPC(Computador computadores) {
+		this.computadores.add(computadores);
+		System.out.println("Computador adicionado!");
+	}
+	
+	public void removePC(int serial) {
+		for (int i = 0; i < computadores.size(); i++) {
+			if(computadores.get(i).getSerial()==serial) {
+				this.computadores.remove(i);
+			}
+		}
+	}
+	
+	public void list() {
+		for (Computador computador : computadores) {
+			computador.exibirPC();
+		}
+	}
+	
+	public void listMarca(String marca) {
+		for (int i = 0; i < computadores.size(); i++) {
+			if(computadores.get(i).getMarca()==marca) {
+				
+			}
+		}
+	}
+	
+	
+	public String getCNPJ() {
+		return CNPJ;
+	}
+	public void setCNPJ(String cNPJ) {
+		CNPJ = cNPJ;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public int getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(int telefone) {
+		this.telefone = telefone;
+	}
+	
+	
+	
+	
+}
