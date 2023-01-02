d822059dfb00a622271e85aa556cefa6374f6363
Luan Moreira <110183580+Amasterluan@users.noreply.github.com>
2023-01-02 18:44:02 -0300

    Update Usuario.java

--- Sistema Completo/Usuario.java
+++ Sistema Completo/Usuario.java
@@ -4,32 +4,34 @@ public class Usuario {
 
 	private int matricula;
 	private String nome;
-	
+
 	public Usuario() {
 		super();
 		this.matricula = matricula;
 		this.nome = nome;
 	}
-	
+
 	public int getMatricula() {
 		return matricula;
 	}
+
 	public void setMatricula(int matricula) {
 		this.matricula = matricula;
 	}
+
 	public String getNome() {
 		return nome;
 	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-	
+
 	public void exibirUsuario() {
-		
+
 		System.out.println("====== USUARIO ======");
 		System.out.println("Matricula: " + this.matricula);
 		System.out.println("Nome: " + this.nome);
 	}
-	
-	
+
 }
